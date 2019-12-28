d3b69106774296ecc57da19f148632080297288d
Nozomu <simaoka@tutanota.com>
2019-12-28 17:42:45 +0900

    Show URLs in order

--- src/Order.js
+++ src/Order.js
@@ -5,19 +5,44 @@ import { withCookies } from 'react-cookie';
 
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 import { bytes } from './utils/human_readable';
 import { order } from './utils/api';
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
 import { API_URL } from './utils/variables';
+
+function URLModal(props) {
+  return (
+    <Modal
+      show={props.show}
+      size='lg'
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>
+          URLs
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form.Control as='textarea' rows={15} value={props.text} readOnly />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 class Order extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       order: null,
+      modal_show: false,
     };
+    this.onHide = this.onHide.bind(this);
   }
   componentDidMount() {
     const user_id = this.props.cookies.get('user_id');
@@ -29,6 +54,12 @@ class Order extends React.Component {
           order,
         });
       }
+    });
+  }
+
+  onHide() {
+    this.setState({
+      modal_show: false,
     });
   }
 
@@ -49,14 +80,22 @@ class Order extends React.Component {
               <td>{item.name}</td>
               <td>{bytes(item.raw_size)}</td>
               <td>{item.count}</td>
-              <td><a href={API_URL + '/get?code=' + item.code} target='_blank' className='btn btn-outline-primary'>Download</a></td>
+              <td>
+                <a href={API_URL + '/get?code=' + item.code}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='btn btn-outline-primary'
+                >
+                  Download
+                </a>
+              </td>
             </tr>
           ))}
           <tr>
             <td>Total</td>
             <td>${this.state.order.total}</td>
             <td></td>
-            <td>List URLs</td>
+            <td><Button onClick={() => this.setState({ modal_show: true })} variant='primary'>List URLs</Button></td>
           </tr>
         </tbody>
       </Table>
@@ -69,16 +108,21 @@ class Order extends React.Component {
     } else {
       return (
         <Container className='Order'>
-          <h1>Order {this.props.order_id}</h1>
+          <h1>Order {this.props.match.params.order_id}</h1>
+          <h2>Date: {moment.unix(this.state.order.date).toString()}</h2>
           <h2>Items</h2>
-          <p>You can use "List URLs" button to show download URLs for all items in this order.</p>
+          <p>You can use 'List URLs' button to show download URLs for all items in this order.</p>
           <p style={{
             fontSize: '2em',
             fontStyle: 'bold',
             color: 'red',
-          }}>Warning: You only have 3 chance to download for each item.</p>
-          <h2>{moment.unix(this.state.order.date).toString()}</h2>
+          }}>Warning: You can only download item for 3 times.</p>
           {this.getTable()}
+          <URLModal
+            show={this.state.modal_show}
+            onHide={this.onHide}
+            text={this.state.order.items.map(item => API_URL + '/get?code=' + item.code).join('\n')}
+          />
         </Container>
       );
     }
