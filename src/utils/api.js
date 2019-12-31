3b4e647e6749cdcc8abf72633bfd6e56a1fa7423
Nozomu <simaoka@tutanota.com>
2019-12-31 13:26:51 +0900

    Conformed to new API

--- src/utils/api.js
+++ src/utils/api.js
@@ -15,51 +15,47 @@ const post = (dir, data) => {
 }
 
 export const cart = (ids) => {
-  return post('/cart', ids).then(res => res.json());
+  return post('/shop/cart', ids).then(res => res.json());
 }
 
 export const search = (query) => {
-  return post('/search', query).then(res => res.json());
+  return post('/shop/search', query).then(res => res.json());
 }
 
 export const signup = (data) => {
-  return post('/signup', data).then(res => res.json());
+  return post('/user/signup', data).then(res => res.json());
 }
 
 export const login = (user_id, password, token) => {
-  return post('/login', {user_id, password, token}).then(res => res.json())
+  return post('/user/login', {user_id, password, token}).then(res => res.json())
 }
 
 export const isLoggedin = (user_id, session_id) => {
-  return post('/isloggedin', {user_id, session_id}).then(res => res.json());
+  return post('/user/isloggedin', {user_id, session_id}).then(res => res.json());
 }
 
 export const logout = (user_id, session_id) => {
-  return post('/logout', {user_id, session_id}).then(res => res.json());
+  return post('/user/logout', {user_id, session_id}).then(res => res.json());
 }
 
 export const bought = (user_id, session_id) => {
-  return post('/bought', {user_id, session_id}).then(res => res.json());
+  return post('/user/bought', {user_id, session_id}).then(res => res.json());
 }
 
 export const purchase = (user_id, session_id, order_id, ids) => {
-  return post('/purchase', {user_id, session_id, order_id, ids}).then(res => res.json());
+  return post('/user/purchase', {user_id, session_id, order_id, ids}).then(res => res.json());
 }
 
 export const listorder = (user_id, session_id) => {
-  return post('/listorder', {user_id, session_id}).then(res => res.json());
+  return post('/user/listorder', {user_id, session_id}).then(res => res.json());
 }
 
 export const order = (user_id, session_id, order_id) => {
   return post('/order', {user_id, session_id, order_id}).then(res => res.json());
 }
 
-export const listdownload = (user_id, session_id) => {
-  return post('/listdownload', {user_id, session_id}).then(res => res.json());
-}
-
 export const sample = (id, name) => {
-  post('/sample', {id})
+  post('/shop/sample', {id})
   .then(res => res.blob())
   .then(blob => download(blob, name, 'text/plain'));
 }
